58acb3c509a1a6f438a9002af690b0b30ad54376
Paul K <pkfis@outlook.com>
2020-01-24 01:24:07 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+  ('51710', 'Sanford, Marquardt and Zieme', 'Medical/Dental Instruments', '$67114.96', '11/13/2018', '4/7/2019'),
+  ('766', 'Weber and Sons', 'n/a', '$34407.39', '9/19/2018', '6/14/2019'),
+  ('8', 'O''Keefe, Lebsack and Wilderman', 'Savings Institutions', '$89839.79', '3/12/2018', '5/29/2019'),
+  ('361', 'Pfannerstill, Gibson and Reinger', 'Other Specialty Stores', '$94020.62', '11/21/2018', '5/28/2019'),
+  ('13', 'Gibson, Kuphal and Walsh', 'n/a', '$91432.75', '2/9/2018', '11/8/2019'),
+  ('3667', 'Jacobson Inc', 'Aerospace', '$5416.87', '7/7/2018', '2/2/2019'),
+  ('857', 'Satterfield Group', 'n/a', '$75124.10', '12/18/2018', '10/9/2019'),
+  ('95', 'Toy Inc', 'Precious Metals', '$13979.16', '6/28/2018', '11/11/2019'),
+  ('3', 'Quigley-Simonis', 'Coal Mining', '$1277.71', '6/18/2018', '2/24/2019'),
+  ('55906', 'Haley, Emard and O''Hara', 'Real Estate Investment Trusts', '$42465.76', '7/9/2018', '8/13/2019');
+
+
+
+  
